77088a3a68bfc224bf4459e3eafb30b74c1f32fc
Samuel Kang <samuel.june.kang@gmail.com>
2025-06-15 04:35:28 -0700

    first

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here!
+
+const endpoint = "https://jsonplaceholder.typicode.com/posts"
+const postList = document.getElementById("post-list")
+
+async function displayPost() {
+    try {
+        const response = await fetch(endpoint);
+        const posts = await response.json();
+
+        posts.forEach(post => {
+            const group = document.createElement('li')
+
+            const header = document.createElement('h1')
+            header.textContent = post.title
+
+            const postText = document.createElement('p')
+            postText.textContent = post.body
+
+            group.appendChild(header)
+            group.appendChild(postText)
+
+            postList.appendChild(group)
+        });
+    } catch (error) {
+        console.error("Failed to fetch posts:", error);
+    }
+}
+
+displayPost()
